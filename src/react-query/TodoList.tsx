2eafdf8073d2ac9b7b3d7a2ab42e31821dc3a71b
Tomas Pesek <TOMP@kea.dk>
2023-05-05 15:48:56 +0200

    handling errors

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -15,12 +15,12 @@ const TodoList = () => {
       .then((res) => res.data);
   };
 
-  const { data: todos } = useQuery({
+  const { data: todos, error } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: () => fetchTodos(),
   });
 
-  //if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
