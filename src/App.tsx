457aed1cb29742262bd434fd8fa456aa4d698201
Tomas Pesek <TOMP@kea.dk>
2023-05-11 22:38:12 +0200

    consolidating state logic with a reducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import PostList from "./react-query/PostList";
 import PostListPaginated from "./react-query/PostListPaginated";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
       {/* <PostList /> */}
       {/* <PostListPaginated /> */}
-      <TodoForm />
-      <TodoList />;
+      {/* <TodoForm />
+      <TodoList />; */}
+      <Counter />
     </>
   );
 }
