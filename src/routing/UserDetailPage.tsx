fe7393d75cb4cf651e0a3b661a459ab6781f4c0b
Tomas Pesek <TOMP@kea.dk>
2023-05-15 23:27:42 +0200

    get data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,17 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log("params: ", params);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log("searchParams: ", searchParams);
+  console.log("searchParams: ", searchParams.toString());
+  console.log("searchParams: ", searchParams.get("age"));
+
+  const location = useLocation();
+  console.log("location: ", location);
+
   return <p>User</p>;
 };
 
